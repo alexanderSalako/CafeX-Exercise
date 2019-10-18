48cd9a0bd668ed5fb35201a8bf9345c756aa4f3a
alexanderSalako <Alexander.salako91@gmail.com>
2019-10-18 12:49:51 +0100

    Step2: Service Charge

--- src/main/scala/CafeX/Solution.scala
+++ src/main/scala/CafeX/Solution.scala
@@ -4,7 +4,7 @@ package CafeX
   * Created By alex on 10/18/2019.
   */
 object Solution {
-  protected def billing(items: List[String]):Double = {
+  protected def billing(items: List[String]) = {
     val d = Map(("Cola",0.50),
       ("Coffee", 1.00),
       ("Cheese Sandwich", 2.00),
@@ -13,7 +13,14 @@ object Solution {
     val prices = for(item <- items) yield d(item)
     val sum_prices = prices.sum
 
-    sum_prices
+    if (items.contains("Steak Sandwich"))
+      if (sum_prices * 0.2 > 20) sum_prices + 20
+      else Math.round((sum_prices + sum_prices * 0.2)* 100)/ 100.0
+    else if (items.contains("Cheese Sandwich"))
+      if (sum_prices * 0.1 > 20) sum_prices + 20
+      else Math.round((sum_prices + sum_prices * 0.1)* 100) / 100.0
+
+    else Math.round(sum_prices * 100)/ 100.0
 
   }
   def main(args: Array[String]): Unit = {
