aea3117520264deb053b1f53142f1d4e07673e65
alexanderSalako <Alexander.salako91@gmail.com>
2019-10-18 13:19:17 +0100

    Step: Adding the documentation

--- src/main/scala/CafeX/Solution.scala
+++ src/main/scala/CafeX/Solution.scala
@@ -4,6 +4,18 @@ package CafeX
   * Created By alex on 10/18/2019.
   */
 object Solution {
+  /** The function takes the items as an argument and returns the total bill
+    * Total bill is calculated by the following conditions:
+    * 1. If all of the items are drink then no service charges.
+    * 2. When purchased items include any food apply a service charge of 10% to the
+    * total bill (rounded to 2 decimal places)
+    * 3. When purchased items include any hot food apply a service charge of 20% to
+    * the total bill
+    * 4. A total of £20 service charge can be added in the total bill.
+    *
+    * @param items the list of all the items
+    * @return the bill including the service charges for the items.
+    */
   def billing(items: List[String]) = {
     val d = Map(("Cola",0.50),
       ("Coffee", 1.00),
